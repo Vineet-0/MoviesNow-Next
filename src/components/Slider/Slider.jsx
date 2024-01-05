59bbdb6685275689b33e348ab302022a09446d99
Vineet-0 <vineet351016@gmail.com>
2024-01-05 17:55:24 +0530

    slider and OTT added

---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,43 @@
+"use client"
+import React, { useEffect, useRef, useState } from 'react'
+import axios from 'axios'
+
+import Carousel from './Carousel.jsx'
+
+const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+const IMAGE_BASE_URL="https://image.tmdb.org/t/p/original";
+
+const Slider = () => {
+    const [movieList,setMovieList]=useState([]);
+
+    useEffect(() => {
+        axios.get("https://api.themoviedb.org/3/trending/all/day?api_key=" + API_KEY)
+          .then(resp => {
+            setMovieList(resp.data.results)
+          })
+          .catch(error => {
+            console.error("Error fetching data:", error);
+          });
+      }, []);
+
+    return (
+        <div className="w-full max-h-[850px] flex flex-center">
+            <Carousel
+                titles={movieList.map((item) => item.title || item.name)}
+                years={movieList.map((item) => item.release_date && item.release_date.slice(0, 4) || item.first_air_date && item.first_air_date.slice(0, 4))}
+                languages={movieList.map((item) => item.original_language)}
+            >
+            {
+                movieList.map((item,index)=>(
+                    <img key={index} src={IMAGE_BASE_URL+item.backdrop_path} 
+                        className='min-w-full object-cover object-left-top'
+                        alt={item.title}
+                    />
+                ))
+            }
+            </Carousel>
+        </div>
+    )
+}
+
+export default Slider
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,15 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Navbar from "../components/Header/Navbar"
+import Slider from "../components/Slider/Slider.jsx"
+import OTT_House from "../components/OTT_House/OTT_House"
+import Footer from "../components/Footer/Footer.jsx"
 
 export default function Home() {
-  return (
-    <div className='w-full min-h-screen bg-red-300 flex items-center justify-center text-9xl text-red-500'>
-      Hello
-    </div>
-  )
+    return (
+        <div className="w-full min-h-screen scrollbar-hide">
+            <Navbar />
+            <Slider />
+            <OTT_House />
+            <Footer />
+        </div>
+    )
 }
